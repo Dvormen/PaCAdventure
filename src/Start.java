816c122ea131bf6ec377105b9f9ebb76fc13d0ea
Dvormen <dvorak13@spsejecna.cz>
2024-05-22 19:25:37 +0200

    Added new ScreenSwaper ss

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,6 @@
 public class Start {
     ActionSolver as = new ActionSolver(this);
+    public ScreenSwaper ss = new ScreenSwaper(this);
     Gui gui = new Gui(this);
     public static void startGame(){
         new Start();
