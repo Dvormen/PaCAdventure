b095be6e3d4f846550dec989c6a728900364ba18
Dvormen <dvorak13@spsejecna.cz>
2024-05-23 20:00:54 +0200

    Added new player

--- src/Start.java
+++ src/Start.java
@@ -1,7 +1,11 @@
 public class Start {
     ActionSolver as = new ActionSolver(this);
     public ScreenSwaper ss = new ScreenSwaper(this);
+    public Player p = new Player(this);
     Gui gui = new Gui(this);
+    public Start() {
+        p.setBasePlayer();
+    }
     public static void startGame(){
         new Start();
     }
