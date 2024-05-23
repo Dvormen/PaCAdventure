91381f2def3bd80eddb38d845c4253ab91be7a39
Dvormen <dvorak13@spsejecna.cz>
2024-05-23 20:01:27 +0200

    Changed all method to work with screen 1

--- src/ScreenSwaper.java
+++ src/ScreenSwaper.java
@@ -7,49 +7,48 @@ public class ScreenSwaper {
 
     public void ScreenM(){
         s.gui.bg[0].setVisible(true);
-        /*s.gui.bg[1].setVisible(false);
-        s.gui.bg[2].setVisible(false);
+        s.gui.bg[1].setVisible(false);
+        /*s.gui.bg[2].setVisible(false);
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);*/
         s.gui.bg[5].setVisible(false);
     }
     public void Screen0(){
         s.gui.bg[0].setVisible(false);
-        /*s.gui.bg[1].setVisible(false);
-        s.gui.bg[2].setVisible(false);
+        s.gui.bg[1].setVisible(false);
+        /*s.gui.bg[2].setVisible(false);
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);*/
         s.gui.bg[5].setVisible(true);
     }
     public void Screen1(){
         s.gui.bg[0].setVisible(false);
-        /*s.gui.bg[1].setVisible(true);
-        s.gui.bg[2].setVisible(false);
+        s.gui.bg[1].setVisible(true);
+        /*s.gui.bg[2].setVisible(false);
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);*/
         s.gui.bg[5].setVisible(false);
     }
     public void Screen2(){
         s.gui.bg[0].setVisible(false);
-        /*s.gui.bg[1].setVisible(false);
-        s.gui.bg[2].setVisible(true);
+        s.gui.bg[1].setVisible(false);
+        /*s.gui.bg[2].setVisible(true);
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);*/
         s.gui.bg[5].setVisible(false);
     }
     public void Screen3(){
         s.gui.bg[0].setVisible(false);
-        /*s.gui.bg[1].setVisible(false);
-        s.gui.bg[2].setVisible(false);
+        s.gui.bg[1].setVisible(false);
+        /*s.gui.bg[2].setVisible(false);
         s.gui.bg[3].setVisible(true);
         s.gui.bg[4].setVisible(false);*/
         s.gui.bg[5].setVisible(false);
     }
     public void Screen4(){
         s.gui.bg[0].setVisible(false);
-        /*
         s.gui.bg[1].setVisible(false);
-        s.gui.bg[2].setVisible(false);
+        /*s.gui.bg[2].setVisible(false);
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(true);*/
         s.gui.bg[5].setVisible(false);
