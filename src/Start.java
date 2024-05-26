cd28a1f00620b511c41d39def23e574b0b8cde34
Dvormen <dvorak13@spsejecna.cz>
2024-05-26 19:23:56 +0200

    actionSolver bits

--- src/Start.java
+++ src/Start.java
@@ -3,6 +3,12 @@ public class Start {
     public ScreenSwaper ss = new ScreenSwaper(this);
     public Player p = new Player(this);
     Gui gui = new Gui(this);
+    public scMActions scm = new scMActions(this);
+    public sc0Actions sc0 = new sc0Actions(this);
+    public sc1Actions sc1 = new sc1Actions(this);
+    public sc2Actions sc2 = new sc2Actions(this);
+    public sc3Actions sc3 = new sc3Actions(this);
+    public sc4Actions sc4 = new sc4Actions(this);
     public Start() {
         ss.ScreenM();
         p.setBasePlayer();
