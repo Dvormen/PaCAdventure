bec35d9241224c06ffa63c7c945b80eb0813279c
Dvormen <dvorak13@spsejecna.cz>
2024-05-27 19:10:34 +0200

    Added sounds

--- src/sc3Actions.java
+++ src/sc3Actions.java
@@ -5,15 +5,18 @@ public class sc3Actions {
     }
     public void talkEnemy(){
         s.gui.textBox.setText("He says that you need a special device to protect yourself\n*You dont understand him but you feel thankful towards him, no need for violence right?*");
+        s.playSound(s.enemy);
     }public void lookEnemy(){
         s.gui.textBox.setText("He looks like a jello on an iron walking machine\n*Looks appetizing*");
+        s.playSound(s.enemy);
     }public void fightEnemy(){
         if(s.p.aqGun){
             s.gui.textBox.setText("Ouch you got me, before you fight the queen you should check her out, you need a special device to beat her.\n*You dont understand a word he just said, but you understood the meaning*");
             s.p.aqArmor = true;
         }else{
-            s.gui.textBox.setText("How?");
+            s.gui.textBox.setText("You dirty cheater! You didnt even take the mission from the general!, Thb congrats this is the true secret :) cant even damage you");
         }
+        s.playSound(s.enemy);
         s.p.checkPlayer();
     }public void exEnemy(){
         if(s.p.currHp ==1){
@@ -24,6 +27,7 @@ public class sc3Actions {
             s.gui.textBox.setText("No petting!");
             s.p.currHp--;
         }
+        s.playSound(s.enemy);
         s.p.checkPlayer();
     }
 }
