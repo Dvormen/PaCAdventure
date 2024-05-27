be8bdcbae8cb829d88ccb8a2f0e6a0465c80211c
Dvormen <dvorak13@spsejecna.cz>
2024-05-27 19:08:57 +0200

    new method musicSwapper

--- src/ScreenSwaper.java
+++ src/ScreenSwaper.java
@@ -1,5 +1,7 @@
+import java.util.Date;
 
 public class ScreenSwaper {
+
     Start s;
     public ScreenSwaper(Start s){
         this.s = s;
@@ -18,13 +20,14 @@ public class ScreenSwaper {
         s.p.setBasePlayer();
     }
 
-    public void ScreenM(){
+    public void ScreenM() throws InterruptedException {
         s.gui.bg[0].setVisible(true);
         s.gui.bg[1].setVisible(false);
         s.gui.bg[2].setVisible(false);
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);
         s.gui.bg[5].setVisible(false);
+        musicSwapper(s.houseMusic);
     }
     public void Screen0(){
         s.gui.bg[0].setVisible(false);
@@ -33,6 +36,8 @@ public class ScreenSwaper {
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);
         s.gui.bg[5].setVisible(true);
+
+        musicSwapper(s.casinoMusic);
     }
     public void Screen1(){
         s.gui.bg[0].setVisible(false);
@@ -41,6 +46,7 @@ public class ScreenSwaper {
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);
         s.gui.bg[5].setVisible(false);
+        musicSwapper(s.airportMusic);
     }
     public void Screen2(){
         s.gui.bg[0].setVisible(false);
@@ -49,6 +55,7 @@ public class ScreenSwaper {
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(false);
         s.gui.bg[5].setVisible(false);
+        musicSwapper(s.spaceship1Music);
     }
     public void Screen3(){
         s.gui.bg[0].setVisible(false);
@@ -57,6 +64,7 @@ public class ScreenSwaper {
         s.gui.bg[3].setVisible(true);
         s.gui.bg[4].setVisible(false);
         s.gui.bg[5].setVisible(false);
+        musicSwapper(s.spaceship2Music);
     }
     public void Screen4(){
         s.gui.bg[0].setVisible(false);
@@ -65,5 +73,14 @@ public class ScreenSwaper {
         s.gui.bg[3].setVisible(false);
         s.gui.bg[4].setVisible(true);
         s.gui.bg[5].setVisible(false);
+        musicSwapper(s.bossMusic);
     }
+    public void musicSwapper(String music) {
+        Date currDate = new Date();
+            if(!s.sc0.jackpotWon && currDate.getTime() - s.sc0.dateJ.getTime() < 251000){
+                s.stopTheMusic();
+                s.musicPlaying = music;
+                s.playMusic(s.musicPlaying);
+    }
+}
 }
