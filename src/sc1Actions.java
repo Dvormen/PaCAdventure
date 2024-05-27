91b78d0cc9f57fec0a0a7e6baad6f75c67cf7207
Dvormen <dvorak13@spsejecna.cz>
2024-05-27 19:11:15 +0200

    Added sounds

--- src/sc1Actions.java
+++ src/sc1Actions.java
@@ -7,9 +7,11 @@ public class sc1Actions {
     }
     public void talkGeneral(){
         s.gui.textBox.setText("Welcome, welcome I have been waiting for you.\n*Interesting*");
+        s.playSound(s.generalHem);
     }
     public void lookGeneral(){
         s.gui.textBox.setText("Looks strong, probably could beat him with a gun or something.\n*Whats that on his arm?*");
+        s.playSound(s.generalGrr);
     }
     public void fightGeneral(){
         if(f == 0){
@@ -17,16 +19,20 @@ public class sc1Actions {
                 s.gui.textBox.setText("Wow such immense strenght! I hereby promote you to a soldier, you should go on an expedition to kill Le Evil Alien, but you can do that because i lost the keys to the space ship, you need to find them");
                 mission++;
                 f++;
+                s.playSound(s.generalHem);
             }else if (s.p.currHp ==1){
                 s.gui.textBox.setText("Pathetic, do you think someone this weak can join the army? Hahaha.");
                 s.p.currHp--;
                 s.ss.gameOver(1);
+                s.playSound(s.generalOoh);
             } else {
                 s.gui.textBox.setText("Pathetic, do you think someone this weak can join the army? Hahaha.");
                 s.p.currHp--;
+                s.playSound(s.generalOoh);
             }
         }else{
             s.gui.textBox.setText("You are too strong for the likes of me.\n*Thats what I thought*");
+            s.playSound(s.generalGrr);
         }
         s.p.checkPlayer();
     }
@@ -41,6 +47,7 @@ public class sc1Actions {
             s.gui.textBox.setText("Congratulations I knew you could do it, you should get your medal for heroism in four to five business days\n*Great, you won the game YAY! you can still go around maybe you would wanna visit the terminal or maybe the casino.*");
             s.p.aqMedal = true;
         }
+        s.playSound(s.generalHem);
         s.p.checkPlayer();
     }
 }
