a29eddf2431805d944c53b4bccb90d474a49f650
Dvormen <dvorak13@spsejecna.cz>
2024-05-28 22:23:00 +0200

    added comments edited text displays

--- src/scMActions.java
+++ src/scMActions.java
@@ -3,7 +3,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
-
+/**
+ * This class takes care of what is on ScreenM - House
+ */
 public class scMActions {
     ArrayList<String> text = new ArrayList<>();
     BufferedReader brM;
@@ -18,67 +20,92 @@ public class scMActions {
         }
         this.s = s;
     }
+
+    /**
+     * this method is used when player picks the option to talk with the bed
+     */
     public void talkBed(){
-        s.gui.textBox.setText(text.get(0));
+        s.gui.textBox.setText(text.get(0)+"\n"+text.get(1));
     }
+    /**
+     * this method is used when player picks the option to look at the bed
+     */
     public void lookBed(){
-        s.gui.textBox.setText(text.get(1));
+        s.gui.textBox.setText(text.get(2)+"\n"+text.get(3));
     }
+    /**
+     * this method is used when player picks the option to fight with the bed
+     */
     public void fightBed(){
         if (s.p.currHp ==1){
-            s.gui.textBox.setText(text.get(2));
+            s.gui.textBox.setText(text.get(4));
             s.ss.gameOver(0);
         }else {
-            s.gui.textBox.setText(text.get(3));
+            s.gui.textBox.setText(text.get(5)+"\n"+text.get(6));
         }
         s.p.currHp--;
         s.p.checkPlayer();
     }
+    /**
+     * this method is used when player picks the option to sleep in the bed
+     */
     public void exBed(){
         if(s.p.maxHp > s.p.currHp){
-            s.gui.textBox.setText(text.get(4));
+            s.gui.textBox.setText(text.get(7)+"\n"+text.get(8));
             s.p.currHp = s.p.currHp +1;
         } else {
-            s.gui.textBox.setText(text.get(5));
+            s.gui.textBox.setText(text.get(9)+"\n"+text.get(10));
         }
         s.p.checkPlayer();
     }
+    /**
+     * this method is used when player picks the option to talk with the cat
+     */
     public void talkCat(){
-        s.gui.textBox.setText(text.get(6));
+        s.gui.textBox.setText(text.get(11)+"\n"+text.get(12));
     }
+    /**
+     * this method is used when player picks the option to look at the cat
+     */
     public void lookCat(){
         s.playSound(s.cat);
-        s.gui.textBox.setText(text.get(7));
+        s.gui.textBox.setText(text.get(13)+"\n"+text.get(14));
     }
+    /**
+     * this method is used when player picks the option to fight with the cat
+     */
     public void fightCat(){
         if (iCat > 0){
             s.p.currHp = s.p.currHp -6;
-            s.gui.textBox.setText(text.get(8));
+            s.gui.textBox.setText(text.get(15)+"\n"+text.get(16));
             s.ss.gameOver(0);
         }
         if (iCat == 0 && s.p.currHp <= 4) {
-            s.gui.textBox.setText(text.get(9));
+            s.gui.textBox.setText(text.get(17)+"\n"+text.get(18));
             s.p.currHp = s.p.currHp -4;
             s.ss.gameOver(0);
         } else if (iCat ==0){
-            s.gui.textBox.setText(text.get(9));
+            s.gui.textBox.setText(text.get(17)+"\n"+text.get(18));
             s.p.currHp = s.p.currHp -4;
         }
         s.playSound(s.cat);
         iCat++;
         s.p.checkPlayer();
     }
+    /**
+     * this method is used when player picks the option to pet the cat
+     */
     public void exCat(){
         int cat = rCat.nextInt(3);
         if ( cat== 2 && s.p.currHp ==1) {
-            s.gui.textBox.setText(text.get(10));
+            s.gui.textBox.setText(text.get(19)+"\n"+text.get(20));
             s.p.currHp--;
             s.ss.gameOver(0);
         } else if (cat == 2) {
-            s.gui.textBox.setText(text.get(10));
+            s.gui.textBox.setText(text.get(19)+"\n"+text.get(20));
             s.p.currHp--;
         } else if(s.p.currHp+2 <= s.p.maxHp){
-            s.gui.textBox.setText(text.get(11));
+            s.gui.textBox.setText(text.get(21)+"\n"+text.get(22));
             for (int i = 0;i !=2;i++){
                 s.p.currHp++;
             }
@@ -88,27 +115,39 @@ public class scMActions {
         s.playSound(s.cat);
         s.p.checkPlayer();
     }
+    /**
+     * this method is used when player picks the option to talk with the crate
+     */
     public void talkCrate(){
-        s.gui.textBox.setText(text.get(12));
+        s.gui.textBox.setText(text.get(23)+"\n"+text.get(24));
     }
+    /**
+     * this method is used when player picks the option to look at the crate
+     */
     public void lookCrate(){
-        s.gui.textBox.setText(text.get(13));
+        s.gui.textBox.setText(text.get(25)+"\n"+text.get(26));
     }
+    /**
+     * this method is used when player picks the option to fight with the bed
+     */
     public void fightCrate(){
-        s.gui.textBox.setText(text.get(14));
+        s.gui.textBox.setText(text.get(27)+"\n"+text.get(28));
     }
+    /**
+     * this method is used when player picks the option to kick the crate
+     */
     public void exCrate(){
         if (!s.p.aqGun && s.p.currHp ==1) {
-            s.gui.textBox.setText(text.get(15));
+            s.gui.textBox.setText(text.get(29)+"\n"+text.get(30));
             s.p.currHp--;
             s.p.aqGun = true;
             s.ss.gameOver(0);
         } else if(!s.p.aqGun){
-            s.gui.textBox.setText(text.get(15));
+            s.gui.textBox.setText(text.get(29)+"\n"+text.get(30));
             s.p.currHp--;
             s.p.aqGun = true;
         }else{
-            s.gui.textBox.setText(text.get(16));
+            s.gui.textBox.setText(text.get(31)+"\n"+text.get(32));
         }
         s.p.checkPlayer();
     }
