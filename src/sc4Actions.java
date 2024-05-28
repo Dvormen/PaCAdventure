0171c05b0495128f53280ffdebf358175c54e99b
Dvormen <dvorak13@spsejecna.cz>
2024-05-28 22:23:31 +0200

    added comments

--- src/sc4Actions.java
+++ src/sc4Actions.java
@@ -2,27 +2,41 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
+/**
+ * This class takes care of what is on Screen4 - Enemy base
+ */
 public class sc4Actions {
     Start s;
     boolean bossIsSlain = false;
     ArrayList<String> text = new ArrayList<>();
     BufferedReader br4;
     public sc4Actions(Start s) throws IOException {
-        br4 = new BufferedReader(new FileReader("textfiles/text3.txt"));
+        br4 = new BufferedReader(new FileReader("textfiles/text4.txt"));
         String temp;
         while((temp = br4.readLine())!=null){
             text.add(temp);
         }
         this.s = s;
     }
+    /**
+     * This method is used when player picks the option to talk with the boss
+     */
     public void talkBoss(){
         s.gui.textBox.setText(text.get(0));
         s.playSound(s.boss);
-    }public void lookBoss(){
+    }
+    /**
+     * This method is used when player picks the option to look at the boss
+     */
+    public void lookBoss(){
         s.gui.textBox.setText(text.get(1));
         s.playSound(s.boss);
-    }public void fightBoss(){
+    }
+
+    /**
+     * This method is used when player picks the option to fight with the boss
+     */
+    public void fightBoss(){
         if(s.p.aqArmor && s.p.aqDevice && !bossIsSlain){
             s.gui.textBox.setText(text.get(2));
             s.sc1.mission++;
@@ -36,7 +50,11 @@ public class sc4Actions {
         }
         s.playSound(s.boss);
         s.p.checkPlayer();
-    }public void exBoss(){
+    }
+    /**
+     * This method is used when player picks the option to ask the boss an important question
+     */
+    public void exBoss(){
         s.gui.textBox.setText(text.get(5));
         s.p.aqMeme = true;
         s.playSound(s.boss);
